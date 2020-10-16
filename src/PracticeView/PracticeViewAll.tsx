3e1d67992bc6daa826e24cbb58e2e1ab7cf852c6
benjaponum <benjaponum@gmail.com>
2020-10-16 22:08:40 +0900

    removed PracticeViewAll.tsx

--- src/PracticeView/PracticeViewAll.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {PracticeViewBase} from "./PracticeViewBase";
-import {loadAllWords} from "../data/Word";
-
-
-export const PracticeViewAll: React.FC = () => {
-  const words = loadAllWords();
-  return <PracticeViewBase words={words}/>
-}
