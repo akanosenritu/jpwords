a061826c5ca9c5ce380d4c0e552f8fcb46597eed
benjaponum <benjaponum@gmail.com>
2020-10-17 00:01:13 +0900

    separeted styles

---
+++ src/PracticeView/PracticeViewStyle.tsx
@@ -0,0 +1,10 @@
+import {makeStyles} from "@material-ui/core/styles";
+
+export const usePracticeViewStyles = makeStyles({
+  startPractice: {
+    border: "1px black solid",
+    borderRadius: "5px / 5px",
+    textAlign: "center",
+    width: "90%"
+  }
+})
