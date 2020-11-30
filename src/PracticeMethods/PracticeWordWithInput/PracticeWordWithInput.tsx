8f82ad99a1fe5da5cb6f5d7d56f2f4d242a3cdad
benjaponum <benjaponum@gmail.com>
2020-11-30 17:57:45 +0900

    small changes

--- src/PracticeMethods/PracticeWordWithInput/PracticeWordWithInput.tsx
+++ src/PracticeMethods/PracticeWordWithInput/PracticeWordWithInput.tsx
@@ -1,43 +1,8 @@
-import React, {ChangeEvent,  useState, useMemo} from "react";
-import {DisplayWordWithFurigana, evaluateAnswer, WordType} from "../../data/Word";
-import {Box, Typography} from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
-import CheckIcon from '@material-ui/icons/Check';
-import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
-import * as wanakana from "wanakana";
-import {getColors} from "../../WordsView/Styles";
+import React, {useState} from "react";
+import {WordType} from "../../data/Word";
 import {initialConfigurations, useConfigurations} from "../../data/Storage/Configurations";
 import {Try} from "./Try";
 import {Final} from "./Final";
-
-const useStyles = makeStyles({
-  answerInput: {
-    width: "100%",
-    height: 24,
-    fontSize: 18,
-    border: "none",
-    outline: "none",
-    boxSizing: "border-box",
-    background: "none",
-    padding: 0
-  },
-  answerInputBox: {
-    width: "80%",
-    display: "flex",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    margin: "auto",
-    border: "2px solid #ddd",
-    borderRadius: 32,
-    borderSizing: "border-box",
-    height: 56,
-    padding: "4px 16px"
-  },
-  answerInputIcon: {
-    padding: "5px 5px",
-    color: "green"
-  }
-});
 
 type PracticeWithInputProps = {
   word: WordType,
@@ -48,7 +13,6 @@ type PracticeWithInputStatusType = "FIRST" | "SECOND" | "SUCCESS" | "FAIL"
 
 export const PracticeWordWithInput: React.FC<PracticeWithInputProps> = (props) => {
   const {configurations} = useConfigurations(initialConfigurations);
-  const [didAnswerWrongly, setDidAnswerWrongly] = useState(false);
   const [status, setStatus] = useState<PracticeWithInputStatusType>("FIRST")
   const [userInput, setUserInput] = useState("")
   return <>
